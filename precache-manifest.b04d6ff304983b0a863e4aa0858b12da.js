4be9c71d36108b3207afaf0dc6a0e1a0abcb08ac
syntax-js <i.rajeshkumar14@gmail.com>
2020-07-21 05:00:56 +0530

    Updates

---
+++ precache-manifest.b04d6ff304983b0a863e4aa0858b12da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bec5613e43200f4602c01ec70a58dd08",
+    "url": "/react_redux-ts/index.html"
+  },
+  {
+    "revision": "c3749f5b4fe67d32ec2e",
+    "url": "/react_redux-ts/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "9554cf55b3624e611aa8",
+    "url": "/react_redux-ts/static/css/main.e79f5d88.chunk.css"
+  },
+  {
+    "revision": "c3749f5b4fe67d32ec2e",
+    "url": "/react_redux-ts/static/js/2.a087567d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react_redux-ts/static/js/2.a087567d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9554cf55b3624e611aa8",
+    "url": "/react_redux-ts/static/js/main.a56df4a5.chunk.js"
+  },
+  {
+    "revision": "1e87dfa494c56b26c1aa",
+    "url": "/react_redux-ts/static/js/runtime-main.439e68a3.js"
+  }
+]);
